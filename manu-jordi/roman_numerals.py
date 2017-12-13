cdfc19bc596eb7f460f47879e0455b9439bd5aaa
Jordi Llull <jordi@jorrdillull.com>
2017-12-13 20:32:26 +0100

    Green test for tripling roman letters

--- manu-jordi/roman_numerals.py
+++ manu-jordi/roman_numerals.py
@@ -2,7 +2,7 @@
 
 class RomanNumerals:
     def convert_from_integer(self, integer):
-        d = {1: "I", 2: "II", 5: "V", 10: "X"}
+        d = {1: "I", 2: "II", 3: "III", 5: "V", 10: "X"}
 
         return d[integer]
 
