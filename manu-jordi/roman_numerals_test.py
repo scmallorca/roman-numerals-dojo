bcec9021d7805e18b0ddc9a1659975eac89658d6
Jordi Llull <jordi@jorrdillull.com>
2017-12-13 20:42:40 +0100

    Extends previous test (red)

--- manu-jordi/roman_numerals_test.py
+++ manu-jordi/roman_numerals_test.py
@@ -24,10 +24,11 @@ class RomanNumeralsTest(unittest.TestCase):
         self.assertEqual(self.roman_numerals.convert_from_integer(10), 'X')
 
     def test_roman_letters_can_repeat_three_times(self):
-        one_roman = self.roman_numerals.convert_from_integer(1)
-        three_roman = self.roman_numerals.convert_from_integer(3)
+        for number in [1, 10, 100, 1000]:
+            one_roman = self.roman_numerals.convert_from_integer(number)
+            three_roman = self.roman_numerals.convert_from_integer(number * 3)
 
-        self.assertEqual(three_roman, one_roman*3)
+            self.assertEqual(three_roman, one_roman * 3)
 
 
 if __name__ == '__main__':
