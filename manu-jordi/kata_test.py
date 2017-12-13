661096579e67608a7866b8697f4f139c38d29a52
Jordi Llull <jordi@jorrdillull.com>
2017-12-13 19:57:05 +0100

    Red test

--- manu-jordi/kata_test.py
+++ manu-jordi/kata_test.py
@@ -10,5 +10,8 @@ class KataTests(unittest.TestCase):
     def test_returns_a_string(self):
         self.assertIsInstance(self.kata.get_roman_numeral(1), str)
 
+    def test_one_returns_I(self):
+        self.assertEqual(self.kata.get_roman_numeral(1), 'I')
+
 if __name__ == '__main__':
     unittest.main()
