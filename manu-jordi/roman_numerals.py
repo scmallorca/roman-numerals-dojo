e965353176db617a77df035edb966c9c3629858d
Jordi Llull <jordi@jorrdillull.com>
2017-12-13 20:16:19 +0100

    Green test for 10 → X

--- manu-jordi/roman_numerals.py
+++ manu-jordi/roman_numerals.py
@@ -2,7 +2,9 @@
 
 class RomanNumerals:
     def convert_from_integer(self, integer):
-        if integer >= 5:
+        if integer >= 10:
+            return "X"
+        elif integer >= 5:
             return "V"
         else:
             return "I" * integer
